f27d7dd694f67aa9e667a7aed29b8630dbf7a75a
Sergey Yarkov <serzh.yarkov@gmail.com>
2019-08-31 22:31:37 +0300

    Added section Team

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:true,
-    dots:false,
-    mouseDrag:false,
-    navText : ["<img src='./src/img/main-stock/left-arrow.png' alt='#'>","<img src='./src/img/main-stock/right-arrow.png' alt='#'>"],
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    mouseDrag: false,
+    navText: ["<img src='./src/img/main-stock/left-arrow.png' alt='#'>", "<img src='./src/img/main-stock/right-arrow.png' alt='#'>"],
+    responsive: {
+        0: {
+            items: 1
         },
-        370:{
-            items:2
+        370: {
+            items: 2
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        700:{
-            items:3
+        700: {
+            items: 3
         },
-        1000:{
-            items:3,
-            nav:true
+        1000: {
+            items: 3,
+            nav: true
         }
     }
 })
